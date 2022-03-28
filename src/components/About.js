95ee2d153e03be28ddd247877bccabf7a5955b50
Daniel Escalona <daniel07escalona@gmail.com>
2022-03-28 17:46:30 -0400

    commit tree 88e77b00816a1e754f3f9d55aa370aa78baa8473

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,35 @@
 import React from "react";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(props) {
+
+  if (props.bio === null || props.bio === ''){
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links />
+      </div>
+    );
+  }else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links />
+      </div>
+    );
+  }
+}
+
+function Links(props){
+  return(
+    <>
+      <h3>Links</h3>
+      <a href={props.links.github}>{props.links.github}</a>
+      <a href={props.links.linkedin}>{props.links.linkedin}</a>
+    </>
+  )
 }
 
 export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,11 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home user/>
+      <About
+        bio={user.bio}
+        links={user.links}
+      />
     </div>
   );
 }
